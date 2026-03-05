1f004a09babed54ca64a52250a6424b258645416
Herapjs <Polaroidploy2538@gmail.com>
2026-03-05 17:27:24 +0700

    final01

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import express from 'express'
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/users.routes.js";
 import dessertsRouter from "./routes/dessert.routes.js";
+import errHandler from "./middlewares/errHandler.js";
 
 
 const app = express()
@@ -16,6 +17,8 @@ app.use("/desserts", dessertsRouter)
 app.use("/reviews", (req, res) => {
     res.send("หิวข้าวแล้ว")
 })
+
+app.use(errHandler)
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`)
